8169898d8e42c38a6e48eb79add6b8f2550f0f7e
zebralt <jocelyn.vernay@ipsos.com>
2020-12-18 13:27:13 +0100

    Straighten setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name = 'piplock',
-    description = 'A minimal command-line tool to lock python requirements files',
-    author = 'vjern',
-    version = '0.1.0',
-    # packages = find_packages(),
-    py_modules = 'piplock',
-    python_requires = '>=3.6',
-    keywords = 'pip, dependency-locking',
-    long_description = open('README.md').read(),
-    long_description_content_type = 'text/markdown',
-    license = 'MIT',
-    url = 'https://github.com/vjern/piplock',
-    entry_points = {
+    name='piplock',
+    description='CLI tool to produce locked python requirements files',
+    author='vjern',
+    version='0.1.0',
+    py_modules='piplock',
+    python_requires='>=3.6',
+    keywords='pip, dependency-locking',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/vjern/piplock',
+    entry_points={
         'console_scripts': ['piplock=piplock.__main__:cli']
     }
 )
